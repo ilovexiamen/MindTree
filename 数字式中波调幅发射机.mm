9baf2e543be89380c07217a101fb9e4a22822095
周宇斌 <76137318+ilovexiamen@users.noreply.github.com>
2022-09-02 13:32:50 +0800

    Update 数字式中波调幅发射机.mm

--- 数字式中波调幅发射机.mm
+++ 数字式中波调幅发射机.mm
@@ -12,19 +12,40 @@
 <node CREATED="1662088559980" ID="ID_259118933" MODIFIED="1662088667869" TEXT="2&#x3001;A/D&#xff08;&#x6a21;&#x62df;&#x6570;&#x5b57;&#xff09;&#x8f6c;&#x6362;&#x5668;&#x5bf9;&#x97f3;&#x9891;&#x4fe1;&#x53f7;&#x8fdb;&#x884c;&#x5feb;&#x901f;&#x8ba1;&#x91cf;&#xff0c;&#x6d4b;&#x51fa;&#x590d;&#x5408;&#x97f3;&#x9891;&#x4fe1;&#x53f7;&#x7684;&#x5927;&#x5c0f;">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1662088668973" ID="ID_1582161022" MODIFIED="1662088668973" TEXT=""/>
+<node CREATED="1662088668973" ID="ID_1582161022" MODIFIED="1662094924684" TEXT="3&#x3001;&#x5c06;&#x89e3;&#x8c03;&#x51fa;&#x6765;&#x7684;2&#x8fdb;&#x5236;&#x6570;&#x8f93;&#x5165;&#x5230;&#x8c03;&#x5236;&#x7f16;&#x7801;&#x5668;&#xff0c;&#x8fdb;&#x884c;&#x63a7;&#x5236;&#x529f;&#x653e;&#x6a21;&#x5757;&#x7684;&#x901a;&#x65ad;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1662096167478" ID="ID_1636949815" MODIFIED="1662096322931" TEXT="4&#x3001;&#x7ecf;&#x8fc7;&#x529f;&#x653e;&#x677f;&#xff08;&#x7b49;&#x538b;&#x529f;&#x653e;&#xff09;&#x548c;&#x4e8c;&#x8fdb;&#x5236;&#x529f;&#x653e;&#x6a21;&#x5757;&#x76f8;&#x7ed3;&#x5408;&#x5b9e;&#x73b0;&#xff08;&#x8f7d;&#x6ce2;&#x7535;&#x538b;&#x9636;&#x68af;&#x6570;&#x548c;&#x590d;&#x5408;&#x97f3;&#x9891;&#x4fe1;&#x53f7;&#x7535;&#x538b;&#x9636;&#x68af;&#x6570;&#x7684;&#x5bf9;&#x5e94;&#x76f8;&#x7b49;*&#xff09;">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1662096278757" ID="ID_428556274" MODIFIED="1662096316825" TEXT="5&#x3001;&#x529f;&#x653e;&#x677f;&#x5408;&#x6210;&#x548c;&#x6ee4;&#x6ce2;&#xff0c;&#x5f62;&#x6210;&#x8c03;&#x5e45;&#x6ce2;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1662096333766" ID="ID_572909812" MODIFIED="1662096362361" TEXT="&#x901a;&#x8fc7;&#x6539;&#x53d8;&#x9ad8;&#x9891;&#x7535;&#x538b;&#x503c;&#x5c31;&#x53ef;&#x4ee5;&#x6539;&#x53d8;&#x53d1;&#x5c04;&#x673a;&#x7684;&#x989d;&#x5b9a;&#x8f93;&#x51fa;&#x529f;&#x7387;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
 <node CREATED="1662088419087" ID="ID_281178234" MODIFIED="1662088441015" TEXT="&#x5e45;&#x76f8;&#x8c03;&#x5236;&#x53d1;&#x5c04;&#x673a;"/>
 </node>
-<node CREATED="1662088710393" ID="ID_1186660567" MODIFIED="1662088769918" POSITION="right" TEXT="&#x4e13;&#x4e1a;&#x672f;&#x8bed;">
+<node CREATED="1662088710393" ID="ID_1186660567" MODIFIED="1662094983281" POSITION="right" TEXT="&#x4e13;&#x4e1a;&#x672f;&#x8bed;">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1662088715885" ID="ID_539761982" MODIFIED="1662089050589" TEXT="&#x9ad8;&#x672b;&#x7ea7;*">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
 <node CREATED="1662089313069" ID="ID_1950732047" MODIFIED="1662089334580" TEXT="A/D&#xff08;&#x6a21;&#x6570;&#xff09;&#x8f6c;&#x6362;&#x5668;">
-<node CREATED="1662089335811" ID="ID_1465024514" MODIFIED="1662089471515" TEXT="&#x5bf9;&#x590d;&#x5408;&#x4fe1;&#x53f7;&#x8fdb;&#x884c;&#x91c7;&#x6837;"/>
+<node CREATED="1662089335811" ID="ID_1465024514" MODIFIED="1662094977877" TEXT="&#x5bf9;&#x590d;&#x5408;&#x4fe1;&#x53f7;&#x8fdb;&#x884c;&#x91c7;&#x6837;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
+<node CREATED="1662094966785" ID="ID_954056930" MODIFIED="1662094979355" TEXT="&#x8f93;&#x51fa;2&#x8fdb;&#x5236;&#x7f16;&#x7801;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1662095319187" ID="ID_545534444" MODIFIED="1662095322912" TEXT="&#x901a;&#x65ad;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1662095324966" ID="ID_880980868" MODIFIED="1662095342906" TEXT="&#x63a7;&#x5236;&#x7535;&#x6d41;&#x7ecf;&#x8fc7;"/>
+</node>
+<node CREATED="1662095345749" ID="ID_1852941994" MODIFIED="1662095345749" TEXT=""/>
 </node>
 </node>
 </map>
