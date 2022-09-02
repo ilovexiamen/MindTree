553b219cf7cd77424e5665575d0a92ce1bf79ca3
周宇斌 <76137318+ilovexiamen@users.noreply.github.com>
2022-09-02 11:51:08 +0800

    2022/9/2 update

---
+++ 数字式中波调幅发射机.mm
@@ -0,0 +1,30 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1662088306119" ID="ID_1977741686" MODIFIED="1662088321795" TEXT="&#x6570;&#x5b57;&#x5f0f;&#x4e2d;&#x6ce2;&#x8c03;&#x5e45;&#x53d1;&#x5c04;&#x673a;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1662088397741" ID="ID_1494398126" MODIFIED="1662088401580" POSITION="right" TEXT="&#x7eea;&#x8bba;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1662088404641" ID="ID_1353476973" MODIFIED="1662088707850" TEXT="DX&#x7cfb;&#x5217;&#x53d1;&#x5c04;&#x673a;">
+<node CREATED="1662088490362" ID="ID_967817263" MODIFIED="1662088508435" TEXT="&#x97f3;&#x9891;&#x53e0;&#x52a0;&#x539f;&#x7406;">
+<node CREATED="1662088526124" ID="ID_530019310" MODIFIED="1662088559481" TEXT="1&#x3001;&#x5c06;&#x76f4;&#x6d41;&#x4fe1;&#x53f7;&#x548c;&#x97f3;&#x9891;&#x4fe1;&#x53f7;&#x53e0;&#x52a0;&#x5728;&#x4e00;&#x8d77;&#xff0c;&#x5f62;&#x6210;&#x590d;&#x5408;&#x97f3;&#x9891;&#x4fe1;&#x53f7;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1662088559980" ID="ID_259118933" MODIFIED="1662088667869" TEXT="2&#x3001;A/D&#xff08;&#x6a21;&#x62df;&#x6570;&#x5b57;&#xff09;&#x8f6c;&#x6362;&#x5668;&#x5bf9;&#x97f3;&#x9891;&#x4fe1;&#x53f7;&#x8fdb;&#x884c;&#x5feb;&#x901f;&#x8ba1;&#x91cf;&#xff0c;&#x6d4b;&#x51fa;&#x590d;&#x5408;&#x97f3;&#x9891;&#x4fe1;&#x53f7;&#x7684;&#x5927;&#x5c0f;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1662088668973" ID="ID_1582161022" MODIFIED="1662088668973" TEXT=""/>
+</node>
+</node>
+<node CREATED="1662088419087" ID="ID_281178234" MODIFIED="1662088441015" TEXT="&#x5e45;&#x76f8;&#x8c03;&#x5236;&#x53d1;&#x5c04;&#x673a;"/>
+</node>
+<node CREATED="1662088710393" ID="ID_1186660567" MODIFIED="1662088769918" POSITION="right" TEXT="&#x4e13;&#x4e1a;&#x672f;&#x8bed;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1662088715885" ID="ID_539761982" MODIFIED="1662089050589" TEXT="&#x9ad8;&#x672b;&#x7ea7;*">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1662089313069" ID="ID_1950732047" MODIFIED="1662089334580" TEXT="A/D&#xff08;&#x6a21;&#x6570;&#xff09;&#x8f6c;&#x6362;&#x5668;">
+<node CREATED="1662089335811" ID="ID_1465024514" MODIFIED="1662089471515" TEXT="&#x5bf9;&#x590d;&#x5408;&#x4fe1;&#x53f7;&#x8fdb;&#x884c;&#x91c7;&#x6837;"/>
+</node>
+</node>
+</node>
+</map>
